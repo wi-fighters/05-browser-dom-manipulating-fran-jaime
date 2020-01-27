391bc0064fbd2243310bdccf329191869e98f7f9
fran <francisco.chiarino@gmail.com>
2020-01-27 14:14:27 +0100

    Ready to push

--- index.js
+++ index.js
@@ -28,11 +28,63 @@ const books = [
   }
 ];
 
-// For each object
+const newArray = books.map(book => {
+  book.author = book.author.split(' ').reverse().join(' ');
+  return book;
+});
 
-  // Create article container
-    // Append Child <img>
-    // Append Child <h3>
-    // Append Child <p>
-    // Append Child <div>
-      // Append Child <span>
+let sortedArray = newArray.sort((a, b) => {
+  if (a.author.charAt(0) > b.author.charAt(0)) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+const ul = document.querySelector('.book-list');
+const liArr = [];
+
+sortedArray.forEach(book => {
+  
+  // Create elements
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.setAttribute('src', book.img);
+
+  // Card elements
+  const h3 = document.createElement('h3');
+  const h3Text = document.createTextNode(book.title);
+  const p = document.createElement('p');
+  const authorArr = book.author.split(' ');
+  const author = `${authorArr[1]}, ${authorArr[0]}`;
+  const pText = document.createTextNode(author);
+  const divSpan = document.createElement('div');
+  const divText = document.createElement('div');
+  const span = document.createElement('span');
+  let spanText = '';
+
+  // Already read
+  if (book.alreadyRead) {
+    spanText = document.createTextNode('Read');
+    span.style.backgroundColor = 'green';
+  } else {
+    spanText = document.createTextNode('To read');
+    span.style.backgroundColor = 'grey';
+  }
+  
+  // Append
+  span.append(spanText);
+  divSpan.append(span);
+  divText.append(h3, p);
+  p.append(pText);
+  h3.append(h3Text);
+  li.append(img, divText, divSpan);
+  ul.append(li);
+
+  // Style
+  span.classList.add('status');
+  img.classList.add('book-cover');
+  divText.classList.add('text');
+  divSpan.classList.add('book-footer');
+  li.classList.add('book')
+})
